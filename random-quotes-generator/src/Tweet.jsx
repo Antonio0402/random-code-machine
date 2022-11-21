7675cba61de252fe7a3b430d6d9bd336b5d5a0bd
Đoàn Thanh Huy <95703775+Antonio0402@users.noreply.github.com>
2022-11-22 01:55:09 +0700

    Delete random-quotes-generator directory

--- random-quotes-generator/src/Tweet.jsx
+++
@@ -1,9 +0,0 @@
-const Tweet = ({quote}) => {
-  return (
-    <a id="tweet-quote" href={`https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${quote}`} target="_blank" rel="noreferrer">
-      <i className="fa-brands fa-twitter"></i>
-    </a>
-  )
-}
-
-export default Tweet;
